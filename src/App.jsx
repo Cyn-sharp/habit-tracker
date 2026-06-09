eb854577097c187d0dd9a3e72a468d13ddd4bcea
Cyn-sharp <cyndrickabejo@gmail.com>
2026-06-09 16:12:41 +0800

    added custom component as button

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import Button from "./components/Button";
+
 export default function App() {
-  return <h1 className="text-red-500">Hello, Vite!</h1>
+  return <div className="max-w-2xl max-auto p-4 flex flex-col gap-4">
+    <Header />
+  </div>
+}
+
+function Header(){
+  return <header className="flex items-center justify-between">
+    <div className="flex flex-col gap-1">
+      <h1 className="text-3xl font-bold">Habit Tracker</h1>
+      <span className="text-zinc-400 text-sm">1 / 1 done today</span>
+    </div>
+    <div className="flex flex-col gap-1">
+      <span className="text-zinc-400 text-sm">Apr 6 - Apr 12</span>
+      <div className="flex items-center gap-3">
+        <Button></Button>
+        <Button></Button>
+      </div>
+    </div>
+  </header>
 }
